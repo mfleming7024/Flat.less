9a75cc46ac36fbd853f10e588b6e5c631c3cf697
Michael Fleming <mfleming7024@gmail.com>
2014-04-17 21:58:12 -0400

    Working on index page and minor styling improvements
    - Updated to new version of Less.js (1.7.0)
    - Formatted color.less and added template for making colors
    - Formatted column.less and added a few width styles
    - Separated all form styles into new form.less
    - Removed all form styles from mixin.less
    - Improved index.html with new visuals/effects
    - Added animations for body on load and the grid system
    - Added some styles for index.html and separated form code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-$("div").each(function() {
+//Grid pixel/class name animation
+$("body").animate({opacity:1}, 2000);
+$("#grid_system .row div").hover(function() {
     var that = $(this);
+    that.find("h2").stop().animate({opacity: 0, left: "20px"}, 400);
+    that.find("h3").stop().animate({opacity: 1, right: "0px"}, 400);
     
+}, function(){
+    var that = $(this);
+    that.find("h2").stop().animate({opacity: 1, left: "0px"}, 400);
+    that.find("h3").stop().animate({opacity: 0, right: "20px"}, 400);
 });
